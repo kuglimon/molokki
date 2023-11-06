1343d026953f21a826ace46f74b957b7184f889b
Tatu Argillander <tatu.argillander@kouralabs.com>
2023-11-06 22:20:59 +0200

    chore: Add CLI and templating support
    Mostly just testing things still. We'll use these to execute the configuration
    once we get that far.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use clap::{Args, Parser, Subcommand};
+use sailfish::TemplateOnce;
+
+/// Rojekti - Tmuxinator but rust
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// List all available projects
+    List {},
+
+    /// Start a tmux session with the given project name
+    Start(StartArgs),
+}
+
+#[derive(Args)]
+struct StartArgs {
+    /// Name of the tmux session and project
+    #[arg(short, long)]
+    name: String,
+}
+
+#[derive(TemplateOnce)]
+#[template(path = "tmux.stpl")]
+struct HelloTemplate {
+    shell: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::List {} => {
+            println!("'myapp add' was used, name is:")
+        }
+        Commands::Start(name) => {
+            let ctx = HelloTemplate {
+                shell: "/bin/bash".to_string(),
+            };
+            println!("{}", ctx.render_once().unwrap());
+        }
+    }
 }
