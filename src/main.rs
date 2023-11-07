0ab33681af38b88ade9c0340bb5d0e0da8f94759
Tatu Argillander <tatu.argillander@kouralabs.com>
2023-11-07 08:58:45 +0200

    cli: Improve layout directory handling
    Instead of hardcoding read them from XDG home and users env. This break when
    HOME is not set, but I have no need to support such systems.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::{Args, Parser, Subcommand};
 use sailfish::TemplateOnce;
 use serde::{Deserialize, Serialize};
+use std::env;
+use std::path::Path;
 use std::{collections::BTreeMap, fs};
 
 /// Rojekti - Tmuxinator but rust
@@ -31,8 +33,8 @@ struct StartArgs {
     name: String,
 }
 
-#[template(path = "tmux.stpl")]
 #[derive(TemplateOnce, Debug, PartialEq, Serialize, Deserialize)]
+#[template(path = "tmux.stpl")]
 struct Config {
     name: String,
     root: Option<String>,
@@ -44,6 +46,12 @@ struct Config {
 fn main() {
     let cli = Cli::parse();
 
+    // TODO(tatu): Maybe move this under environment or some similar struct
+    let home_path = env::var("HOME").expect("HOME is not set on env, cannot continue");
+    // TODO(tatu): Doesn't support .config in another directory, but I never change this, meh.
+    let xdg_config_home = Path::new(&home_path).join(".config");
+    let layout_home = xdg_config_home.join("tmuxinator");
+
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
@@ -51,17 +59,27 @@ fn main() {
             unimplemented!("Add support for listing")
         }
         Commands::Debug(name) => {
-            let contents = fs::read_to_string(
-                "/Users/kuglimon/development/personal/layouts/tmuxinator/dotfiles.yml",
-            )
-            .expect("Should have been able to read the file");
+            let project_file = layout_home.join(&name.name).with_extension("yml");
 
-            let config: Config = serde_yaml::from_str(&contents).unwrap();
+            println!(
+                "Debugging project {}",
+                project_file
+                    .to_str()
+                    .unwrap_or("project file is not a valid path")
+            );
 
-            println!("{:?}", config);
-            println!("{}", config.render_once().unwrap());
+            if project_file.is_file() {
+                let contents = fs::read_to_string(project_file)
+                    .expect("Could not read given project file, check permissions");
+
+                let config: Config = serde_yaml::from_str(&contents).unwrap();
+                println!("{}", config.render_once().unwrap());
+            } else {
+                println!("Given project does not exist or is not a file");
+                // TODO(tatu): We should fall to create in this case
+            }
         }
-        Commands::Start(name) => {
+        Commands::Start(_name) => {
             unimplemented!("Add support for start")
         }
     }
