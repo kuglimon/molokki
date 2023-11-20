f9292c496125dce2c9e74894ef62fe1dbb7b204e
Tatu Argillander <tatu.argillander@kouralabs.com>
2023-11-20 12:11:45 +0200

    rojekti: Support window and pane creation
    Supports basic windows and their implicit panes with commands.
    
    Misc bug fixes.

--- rojekti/templates/tmux.sh
+++ rojekti/templates/tmux.sh
@@ -1,21 +1,37 @@
 #!/usr/bin/env bash
 
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipeset -e
+
+if [ -z ${DEBUG+x} ]; then
+  set -x
+fi
+
 tmux start-server
 
-cd {{ root }} %>
+cd {{ root }}
 
 # Run project on start hooks
 # TODO(tatu): I've yet to use this
 
 if tmux has-session -t "{{ name }}" &>/dev/null; then
-  # TODO(tatu): Needs window indexing
-  echo "new session"
-  {% for window in windows -%}
-    tmux new-window "path" -t #{tmux_window_target} -n {{ window.name }}
-  {% endfor -%}
-else
-  echo "existing session"
   # TODO(tatu): Implement existing session support
+  echo "existing session"
+else
+{% for window in windows -%}
+  {% if loop.index == 1 -%}
+  # XXX(tatu): Why do we reset TMUX?
+  # XXX(tatu): Why does indentation get fucked here by extra level
+  TMUX= tmux new-session -d -s {{ name }} -n {{ window.name }}
+  tmux send-keys -t {{ name }}:{{ loop.index }} cd\ {{ root }} C-m
+  {% else -%}
+    tmux new-window -c {{ root }} -t {{ name }}:{{ loop.index }} -n {{ window.name }}
+  {% endif -%}
+  {% if window.command -%}
+    tmux send-keys -t {{ name }}:{{ loop.index }} {{ window.command }} C-m
+  {% endif -%}
+{% endfor %}
 fi
 
 {% if attach  -%}
