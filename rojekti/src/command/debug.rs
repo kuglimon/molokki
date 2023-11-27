7fba3fa9654dc9c2deec28dfb18ab8667565a74a
Tatu Argillander <tatu.argillander@kouralabs.com>
2023-11-27 20:17:15 +0200

    cli: Fix debug command imports

--- rojekti/src/command/debug.rs
+++ rojekti/src/command/debug.rs
@@ -1,9 +1,10 @@
-use std::io;
+use std::error::Error;
+use std::io::{self, Write};
 
 use crate::{config::Config, project::Project, StartArgs};
 
 pub fn run(config: Config, args: &StartArgs, project_name: &str) -> Result<(), Box<dyn Error>> {
-    let project = Project::load(config, args, &args.name)?;
+    let project = Project::load(config, args, project_name)?;
     writeln!(io::stdout(), "{}", project.render()?)?;
     Ok(())
 }
