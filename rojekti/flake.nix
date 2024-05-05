57e7975431bfe90edf09653f580ba9c6e6f07cf9
Tatu Argillander <tatu.argillander@kouralabs.com>
2024-05-05 13:16:33 +0300

    rojekti: add flake based build and dev shell

---
+++ rojekti/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "rojekti flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    packages.${system}.default = (import ./default.nix { inherit pkgs; });
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        cargo
+      ];
+    };
+  };
+}
