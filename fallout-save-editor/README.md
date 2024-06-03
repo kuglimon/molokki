6aa4dd90e39272a9a67b5426c97760fddbfac118
Tatu Argillander <taatelio@gmail.com>
2024-06-03 23:49:39 +0300

    fallout: start work on editor

---
+++ fallout-save-editor/README.md
@@ -0,0 +1,19 @@
+# Fallout Save Editor
+
+Save game editor for Fallout 2.
+
+Features:
+
+* None, does nothing
+
+Todo:
+
+* Remove NCR aggro
+
+# But why?
+
+Ain't downloading no random ass compiled editor from a mod site.
+
+Also fucking Sulik didn't put his weapon away and now half of NCR want's to kill
+me. Some how I felt the best response would be to make my own editor and
+introduce some binary protocol forced positivity into NCR.
---
+++ fallout-save-editor/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "fallout-save-editor flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+      pname = "fallout-save-editor";
+      version = "0.1.0";
+      cargoLock.lockFile = ./Cargo.lock;
+      src = pkgs.lib.cleanSource ./.;
+    };
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        cargo
+
+        # tmuxinator for testing feature parity
+        (ruby.withPackages (ps: with ps; [ tmuxinator ]))
+      ];
+    };
+  };
+}
