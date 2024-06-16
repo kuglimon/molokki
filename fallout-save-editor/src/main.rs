e8b32589fef73134e1feb7037c32a26ee1acf81d
Tatu Argillander <taatelio@gmail.com>
2024-06-16 18:48:58 +0300

    fallout: add simple terminal ui
    Just reads a save from a path and prints parser results.

--- fallout-save-editor/src/main.rs
+++ fallout-save-editor/src/main.rs
@@ -1,5 +1,6 @@
-use fallout_save_editor::ui::run_ui;
+use fallout_save_editor::ui::run_terminal_ui;
 
 fn main() {
-    run_ui();
+    // run_ui();
+    run_terminal_ui();
 }
--- fallout-save-editor/src/ui.rs
+++ fallout-save-editor/src/ui.rs
@@ -1,3 +1,9 @@
+use std::fs;
+
+use clap::Parser;
+
+use crate::parser::{dat2, try_decompress_dat2};
+
 slint::slint! {
     export component HelloWorld {
         Text {
@@ -9,4 +15,26 @@ slint::slint! {
 
 pub fn run_ui() {
     HelloWorld::new().unwrap().run().unwrap();
+}
+
+// FIXME(tatu): Make a command like fix and options for common fixes
+/// Program to fix NCR guard agro in Fallout 2 saves
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the save file to load
+    #[arg(short, long)]
+    save_file_path: String,
+}
+
+pub fn run_terminal_ui() {
+    let args = Args::parse();
+    let content = fs::read(args.save_file_path).expect("could not read save file");
+
+    let decompressed = try_decompress_dat2(content);
+    let (map_save, map_variables, scripts) = dat2(&decompressed);
+
+    dbg!(map_save);
+    dbg!(map_variables);
+    dbg!(scripts);
 }
