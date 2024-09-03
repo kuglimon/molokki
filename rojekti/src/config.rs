34413aad9599fb2a4e28ee03e8fb17682f672791
Tatu Argillander <taatelio@gmail.com>
2024-09-03 20:59:55 +0300

    rojekti: add tests for runtime config

--- rojekti/src/config.rs
+++ rojekti/src/config.rs
@@ -31,3 +31,62 @@ impl RuntimeEnvironment {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use once_cell::sync::Lazy; // 1.4.0
+    use std::env;
+    use std::sync::Mutex;
+
+    use super::RuntimeEnvironment;
+
+    static THE_RESOURCE: Lazy<Mutex<()>> = Lazy::new(Mutex::default);
+    type TestResult<T = (), E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
+
+    fn reset_env() {
+        env::remove_var("XDG_CONFIG_HOME");
+        env::remove_var("EDITOR");
+        env::remove_var("HOME");
+    }
+
+    #[test]
+    fn it_uses_config_from_environment_variables() -> TestResult {
+        let _shared = THE_RESOURCE.lock()?;
+
+        env::set_var("EDITOR", "vim");
+        env::set_var("HOME", "/tmp");
+
+        let runtime_environment = RuntimeEnvironment::from_env();
+
+        assert_eq!(runtime_environment.editor, "vim");
+        assert_eq!(
+            runtime_environment.layout_path.to_str(),
+            Some("/tmp/.config/rojekti")
+        );
+
+        reset_env();
+
+        Ok(())
+    }
+
+    #[test]
+    fn it_prioritizes_xdg_config_home() -> TestResult {
+        let _shared = THE_RESOURCE.lock()?;
+
+        env::set_var("EDITOR", "vim");
+        env::set_var("HOME", "/tmp");
+        env::set_var("XDG_CONFIG_HOME", "/what/.config");
+
+        let runtime_environment = RuntimeEnvironment::from_env();
+
+        assert_eq!(runtime_environment.editor, "vim");
+        assert_eq!(
+            runtime_environment.layout_path.to_str(),
+            Some("/what/.config/rojekti")
+        );
+
+        reset_env();
+
+        Ok(())
+    }
+}
