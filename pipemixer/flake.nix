80b7104a7fcd1d72b50ef8b1ae3a7c46e9f3adc0
Tatu Argillander <taatelio@gmail.com>
2024-09-09 21:13:59 +0300

    pipemixer: check nix format with alejandra

--- pipemixer/flake.nix
+++ pipemixer/flake.nix
@@ -5,13 +5,14 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-  let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-    cargoToml = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
-  in
-  {
+    cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+  in {
     packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
       pname = cargoToml.package.name;
       version = cargoToml.package.version;
@@ -28,14 +29,23 @@
       LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
     };
 
-    devShells.${system}.default =  (pkgs.mkShell.override { stdenv = pkgs.llvmPackages.stdenv; }) {
+    devShells.${system}.default = (pkgs.mkShell.override {stdenv = pkgs.llvmPackages.stdenv;}) {
       buildInputs = with pkgs; [
         cargo
+        alejandra
         pkg-config
         pipewire.dev
       ];
 
       LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
     };
+
+    checks.x86_64-linux.alejandro = with nixpkgs.legacyPackages.x86_64-linux;
+      runCommand "format" {
+        nativeBuildInputs = with pkgs; [alejandra];
+      } ''
+        alejandra -c ${self}
+        touch $out
+      '';
   };
 }
