b6d5c60fc21d0dd7fd8f646752e443ea8bf30b56
Tatu Argillander <taatelio@gmail.com>
2024-09-19 19:56:08 +0300

    build(rojekti): merge default.nix to flake.nix
    Other projects in molokki do the same

--- rojekti/flake.nix
+++ rojekti/flake.nix
@@ -10,9 +10,28 @@
     nixpkgs,
   }: let
     system = "x86_64-linux";
+    cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-    packages.${system}.default = import ./default.nix {inherit pkgs;};
+    packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+      pname = cargoToml.package.name;
+      version = cargoToml.package.version;
+      buildInputs = with pkgs; [
+        pipewire.dev
+      ];
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+        rustPlatform.bindgenHook
+      ];
+      cargoLock.lockFile = ./Cargo.lock;
+      src = pkgs.lib.cleanSource ./.;
+
+      LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+
+      checkPhase = ''
+        cargo test
+      '';
+    };
 
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
