67d8bb61b37cacbd31b4484ad8938619901a531b
Esco441-91 <esa.laakso91@gmail.com>
2025-01-21 23:06:06 +0200

    attach: Add a attach function and logging
    Just to make the difference between memory spaces clear, add an attach
    function. It should be used as the entry point for anything.
    
    As the first step, add a logger and log the attach function. They will
    let us know when we've made first contact with the game.

--- swkotor-mod/src/lib.rs
+++ swkotor-mod/src/lib.rs
@@ -1,15 +1,35 @@
-use windows::{Win32::Foundation::*, Win32::System::SystemServices::*};
+pub mod entry;
+
+use windows::Win32::Foundation::HINSTANCE;
+use windows::Win32::System::SystemServices::*;
+use env_logger::{self, Env};
+use log::trace;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
+}
+
+fn setup_logging() {
+    // Dump all logs to a file. For that, we'll need a pipe to pass to env_logger.
+    let file = std::fs::File::create("swkotor-mod.log").expect("Failed to initialize logging file for piping.");
+    let mut builder = env_logger::Builder::from_env(Env::default().default_filter_or("trace"));
+    builder.target(env_logger::Target::Pipe(Box::new(file)));
+    builder.init();
+}
+
+fn attach() {
+    setup_logging();
+    trace!("Hello from the DLL!");
 }
 
 #[no_mangle]
 #[allow(non_snake_case, unused_variables)]
 extern "system" fn DllMain(dll_module: HINSTANCE, call_reason: u32, _: *mut ()) -> bool {
     match call_reason {
-        DLL_PROCESS_ATTACH => println!("dll attach"),
-        DLL_PROCESS_DETACH => println!("dll deattach"),
+        DLL_PROCESS_ATTACH => {
+            attach();
+        },
+        DLL_PROCESS_DETACH => (),
         _ => (),
     }
 
