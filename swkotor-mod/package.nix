e49d9208030ae7b1da29f43cff8a75ba891bc995
Tatu Argillander <taatelio@gmail.com>
2025-01-25 17:54:38 +0200

    swkotor: add launcher script
    Nixified launcher script for starting the game through terminal.
    
    The steam web protocol arguments don't seem to be the same as launch
    options. Launch options can affect how the whole executable is started,
    through use of %command%. Where the launch options just seem to be
    arguments passed down to the executable.
    
    I'll have to ask Valve if this is even supported or if there are other
    options to do this.

--- swkotor-mod/package.nix
+++ swkotor-mod/package.nix
@@ -2,6 +2,8 @@
   pkgs,
   crane,
   fenix,
+  writeTextFile,
+  symlinkJoin,
   # XXX(tatu): Am I supposed to pass system like this? Maybe this should be
   # higher up
   system
@@ -36,32 +38,89 @@ let
     ];
 
   craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+
+  swkotor-mod = craneLib.buildPackage rec {
+    src = craneLib.cleanCargoSource ./.;
+
+    strictDeps = true;
+    doCheck = false;
+
+    CARGO_BUILD_TARGET = "i686-pc-windows-gnu";
+
+    TARGET_CC   = "${winCC}/bin/${winCC.targetPrefix}cc";
+
+    CARGO_BUILD_RUSTFLAGS = [
+      "-C"
+      "linker=${TARGET_CC}"
+    ];
+
+    # fixes issues related to openssl
+    OPENSSL_DIR = "${pkgs.openssl.dev}";
+    OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+    OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include/";
+
+    CC = "${winCC}/bin/${winCC.targetPrefix}cc";
+    LD = "${winCC}/bin/${winCC.targetPrefix}cc";
+
+    depsBuildBuild = with pkgs; [
+      pkgsCross.mingw32.stdenv.cc
+      pkgsCross.mingw32.windows.pthreads
+    ];
+  };
+
+  # Helper script for launching kotor directly through steam with correct dll in
+  # place and all. Note that this script is impure, it assumes you have steam
+  # installed and kotor installed.
+  #
+  # We are using 'writeTextFile' because the bash environment nix would run this
+  # in isn't usable in steam context. It's likely fixable, but I'm just cutting
+  # corners here. I couldn't get the automation working either.
+  #
+  # To use this set this script as the launch option like so:
+  #   <path_to_repo>/.result/bin/run-swkotor-mod %command%
+  runSWKotorMod = writeTextFile {
+    name = "run-swkotor-mod";
+    text = ''
+      #!/usr/bin/env bash
+      LOG_FILE="$HOME/.steam-debugger.log"
+
+      # Redirect IO to file
+      exec >> "$LOG_FILE"
+
+      # If we have no command then ask steam to start the game using this script
+      if [ -z "$1" ]; then
+        # Get our path and pass it to steam
+        SCRIPT_PATH="$(dirname $(realpath -s $0))/run-swkotor-mod"
+        URL_SAFE_COMMAND="$(printf %s "$SCRIPT_PATH %command%"|${pkgs.jq}/bin/jq -sRr @uri)"
+
+        echo "Make steam start kotor with arguments '$SCRIPT_PATH %command%'"
+
+        # FIXME(tatu): This does not work
+        # Start our kotor and make launch options point to this script.
+        # steam "steam://run/32370//$URL_SAFE_COMMAND/"
+        exit 0
+      fi
+
+      echo "Starting debugging steam game with command:"
+      echo "$@"
+
+      # Force steam to load our modified dll
+      # FIXME(tatu): Can this be an absolute path?
+      export WINEDLLOVERRIDES="dinput8.dll=n"
+
+      # Copy our built dll to the game install directory under steam.
+      cp "${swkotor-mod}/lib/swkotor_mod.dll" ~/.local/share/Steam/steamapps/common/swkotor/dinput8.dll
+
+      echo "DLL copied, starting kotor"
+
+      # MANGOHUD_CONFIG=fps_limit=60 mangohud
+      "$@"
+    '';
+    executable = true;
+    destination = "/bin/run-swkotor-mod";
+  };
 in
-craneLib.buildPackage rec {
-  src = craneLib.cleanCargoSource ./.;
-
-  strictDeps = true;
-  doCheck = false;
-
-  CARGO_BUILD_TARGET = "i686-pc-windows-gnu";
-
-  TARGET_CC   = "${winCC}/bin/${winCC.targetPrefix}cc";
-
-  CARGO_BUILD_RUSTFLAGS = [
-    "-C"
-    "linker=${TARGET_CC}"
-  ];
-
-  #fixes issues related to openssl
-  OPENSSL_DIR = "${pkgs.openssl.dev}";
-  OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
-  OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include/";
-
-  CC = "${winCC}/bin/${winCC.targetPrefix}cc";
-  LD = "${winCC}/bin/${winCC.targetPrefix}cc";
-
-  depsBuildBuild = with pkgs; [
-    pkgsCross.mingw32.stdenv.cc
-    pkgsCross.mingw32.windows.pthreads
-  ];
+symlinkJoin {
+  name = "swkotor-mod";
+  paths = [ swkotor-mod runSWKotorMod ];
 }
