7ca3bc7138e25ca4888580445506e93b580477d9
Tatu Argillander <taatelio@gmail.com>
2025-04-13 11:24:19 +0300

    flake: find nix projects automatically
    Detect presence of `package.nix` in a subdirectory and load it. Removing
    the need to define each project by hand.
    
    Use `callPackageWith` to create a custom `callPackage` with `inputs` in
    scope to remove need for manually passing inputs to projects.
    
    I'm still on the fence if this is just utter wankery and will this make
    it harder to reason about projects. The intend is to merge `package.nix`
    and `shell.nix` within a project to one file and pass these to the root
    flake. Having this automatic wankery should make that setup simpler,
    with the cost of making code harder to read: Nix REPL is probably the
    only sane way to reason at that point.

--- flake.nix
+++ flake.nix
@@ -18,12 +18,47 @@
       flake-utils,
       crane,
       fenix,
-    }:
+    } @ inputs:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         lib = pkgs.lib;
+        callPackage = pkgs.lib.callPackageWith (pkgs // inputs);
+        root = ./.;
+
+        # Find all nix based projects, not all have been converted
+        nixProjects = lib.filterAttrs (name: type:
+          type == "directory" &&
+          builtins.pathExists (root + "/${name}/package.nix")
+        ) (builtins.readDir root);
+
+        # Import all projects and separate derivation and attrset based ones.
+        # Derivations are the old way I used to use where package.nix and
+        # shell.nix were separate files.
+        #
+        # Using separate files lead to issues where it was harder to share
+        # configuration with package.nix and shell.nix. To be fair, most of that
+        # was just me being fucking stupid. But it was annoying to flick around
+        # the files.
+        #
+        # Remember that there's not really much magic in nix, it's just a
+        # programming language with some agreed shape of attributes. Learn the
+        # difference between attribute sets, derivations and functions, you're
+        # 95% done after that.
+        #
+        # FIXME(tatu): evaluated, derivations and attrsets could be done in one
+        # statement, this is just overly complex.
+        evaluated = lib.mapAttrs (name: _:
+            # Note that this requires a callPackage that has inputs as default
+            # attrs, as we have packages depending on inputs like fenix and
+            # crane
+            callPackage (root + "/${name}/package.nix") { }
+        ) nixProjects;
+
+        # Split into derivations and non-derivation attrsets
+        derivations = lib.filterAttrs (_: v: lib.isDerivation v) evaluated;
+        attrsets = lib.filterAttrs (_: v: builtins.isAttrs v && !lib.isDerivation v) evaluated;
       in
       {
         # FIXME(tatu): Move configuration back to subdirectories and use
@@ -31,16 +66,8 @@
         # XXX(tatu): I wonder if it would be easier to move all projects under
         #            something like pkgs?
         packages = rec {
-          fallout-save-editor = pkgs.callPackage ./fallout-save-editor/package.nix { };
-          rojekti = pkgs.callPackage ./rojekti/package.nix { };
-          pipemixer = pkgs.callPackage ./pipemixer/package.nix { };
-          poe-trade-overlay = pkgs.callPackage ./poe-trade-overlay/package.nix { };
-          swkotor-mod = pkgs.callPackage ./swkotor-mod/package.nix {
-            inherit crane;
-            inherit fenix;
-          };
-
-          default = rojekti;
+          # XXX(tatu): Should the default just build everything?
+          default = self.packages.${system}.rojekti;
 
           # This output is used to generate the job matrix in github to build
           # each output separately.
@@ -54,7 +81,7 @@
               os = [ "ubuntu-latest" ];
             }
           );
-        };
+        } // derivations;
 
         formatter = pkgs.nixfmt-rfc-style;
 
