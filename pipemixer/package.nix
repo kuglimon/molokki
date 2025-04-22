277e484bb21463236a8586c9e983d156e24214ed
Tatu Argillander <taatelio@gmail.com>
2025-04-22 20:50:03 +0300

    pipemixer: move to attribute based setup

--- pipemixer/package.nix
+++ pipemixer/package.nix
@@ -4,22 +4,35 @@
 let
   cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
 in
-pkgs.rustPlatform.buildRustPackage {
-  pname = cargoToml.package.name;
-  version = cargoToml.package.version;
-  buildInputs = with pkgs; [
-    pipewire.dev
-  ];
-  nativeBuildInputs = with pkgs; [
-    pkg-config
-    rustPlatform.bindgenHook
-  ];
-  cargoLock.lockFile = ./Cargo.lock;
-  src = pkgs.lib.cleanSource ./.;
+{
+  package = pkgs.rustPlatform.buildRustPackage {
+    pname = cargoToml.package.name;
+    version = cargoToml.package.version;
+    buildInputs = with pkgs; [
+      pipewire.dev
+    ];
+    nativeBuildInputs = with pkgs; [
+      pkg-config
+      rustPlatform.bindgenHook
+    ];
+    cargoLock.lockFile = ./Cargo.lock;
+    src = pkgs.lib.cleanSource ./.;
 
-  LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+    LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
 
-  checkPhase = ''
-    cargo test
-  '';
+    checkPhase = ''
+      cargo test
+    '';
+  };
+
+  devShell = (pkgs.mkShell.override { stdenv = pkgs.llvmPackages.stdenv; }) {
+    buildInputs = with pkgs; [
+      cargo
+      alejandra
+      pkg-config
+      pipewire.dev
+    ];
+
+    LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+  };
 }
