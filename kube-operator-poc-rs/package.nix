0400fc03391a7982de7dfdfd4a61dfa29836a0d7
Tatu Argillander <taatelio@gmail.com>
2025-05-04 11:06:25 +0300

    k8s(operator): fix building workspaces
    Nix build was broken as I had moved to workspaces.

--- kube-operator-poc-rs/package.nix
+++ kube-operator-poc-rs/package.nix
@@ -7,9 +7,10 @@
   system
 }:
 let
+  inherit (pkgs) lib;
+
   craneLib = crane.mkLib pkgs;
-  src = craneLib.cleanCargoSource ./krangle-api;
-  operatorSrc = craneLib.cleanCargoSource ./krangle-operator;
+  src = craneLib.cleanCargoSource ./.;
 
   # Common arguments can be set here to avoid repeating them later
   commonArgs = {
@@ -24,25 +25,48 @@ let
     # MY_CUSTOM_VAR = "some value";
   };
 
-  operatorArgs = {
-    src = operatorSrc;
-    strictDeps = true;
-  };
-
-  # Build *just* the cargo dependencies, so we can reuse
-  # all of that work (e.g. via cachix) when running in CI
+  # Build *just* the cargo dependencies (of the entire workspace),
+  # so we can reuse all of that work (e.g. via cachix) when running in CI
+  # It is *highly* recommended to use something like cargo-hakari to avoid
+  # cache misses when building individual top-level-crates  cargoArtifacts = craneLib.buildDepsOnly commonArgs;
   cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-  operatorCargoArtifacts = craneLib.buildDepsOnly operatorArgs;
-
-  # Build the actual crate itself, reusing the dependency
-  # artifacts from above.
-  krangle-api-crate = craneLib.buildPackage (commonArgs // {
+  individualCrateArgs = commonArgs // {
     inherit cargoArtifacts;
+    inherit (craneLib.crateNameFromCargoToml { inherit src; }) version;
+    # NB: we disable tests since we'll run them all via cargo-nextest
+    doCheck = false;
+  };
+
+  fileSetForCrate = crate: lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.unions [
+      ./Cargo.toml
+      ./Cargo.lock
+      (craneLib.fileset.commonCargoSources ./crates/krangle-api)
+      (craneLib.fileset.commonCargoSources ./crates/krangle-operator)
+      (craneLib.fileset.commonCargoSources crate)
+    ];
+  };
+
+  # Build the top-level crates of the workspace as individual derivations.
+  # This allows consumers to only depend on (and build) only what they need.
+  # Though it is possible to build the entire workspace as a single derivation,
+  # so this is left up to you on how to organize things
+  #
+  # Note that the cargo workspace must define `workspace.members` using wildcards,
+  # otherwise, omitting a crate (like we do below) will result in errors since
+  # cargo won't be able to find the sources for all members.
+  krangle-api-crate = craneLib.buildPackage (individualCrateArgs // {
+    pname = "krangle-api";
+    cargoExtraArgs = "-p krangle-api";
+    src = fileSetForCrate ./crates/krangle-api;
   });
 
-  krangle-operator-crate = craneLib.buildPackage (operatorArgs // {
-    cargoArtifacts = operatorCargoArtifacts;
+  krangle-operator-crate = craneLib.buildPackage (individualCrateArgs // {
+    pname = "krangle-operator";
+    cargoExtraArgs = "-p krangle-operator";
+    src = fileSetForCrate ./crates/krangle-operator;
   });
 
   krangle-api-image = pkgs.dockerTools.buildLayeredImage {
