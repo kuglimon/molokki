c58781978c28810f8069dc2c50476d00323c5f2a
Tatu Argillander <taatelio@gmail.com>
2026-02-16 21:09:17 +0200

    brosh: simplify testing bash
    Don't try to start with the perfect solution. Just add the required
    dependencies on the devshell and build bash using that.
    
    There's time to optimize later.

--- brosh/package.nix
+++ brosh/package.nix
@@ -4,15 +4,13 @@
 let
   cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
 
-  bashWithTests = import ./pkgs/bash { inherit pkgs; };
-
   run-all-tests = pkgs.writeShellApplication {
     name = "run-tests.sh";
 
     runtimeInputs = [
     ];
 
-    text = builtins.readFile ./run-tests.sh;
+    text = builtins.readFile ./devtools/run-tests.sh;
   };
 in
 {
@@ -41,6 +39,65 @@ in
     buildInputs = [
     ];
 
-    packages = [ run-all-tests ];
+    packages = with pkgs; [
+      run-all-tests
+
+      # Following dependencies used for building bash
+      # Build tools
+      autoconf
+      automake
+      m4
+      bison
+      flex
+      gnumake
+      gcc
+      pkg-config
+
+      # Required libraries
+      readline
+      ncurses
+      gettext
+
+      # Documentation
+      texinfo
+      groff
+
+      # Testing/debugging
+      gdb
+      valgrind
+      diffutils
+
+      # generates compile_commands.json for clangd
+      bear
+
+      # Useful utilities
+      git
+      less
+    ];
+
+    shellHook = ''
+      echo "GNU Bash development environment"
+      echo ""
+      echo "Build instructions:"
+      echo "  # If configure doesn't exist (git clone):"
+      echo "  autoconf"
+      echo ""
+      echo "  # Build with LSP support (generates compile_commands.json):"
+      echo "  ./configure"
+      echo "  bear -- make -j\$(nproc)"
+      echo ""
+      echo "  # Or plain build:"
+      echo "  ./configure && make -j\$(nproc)"
+      echo ""
+      echo "  # Run tests:"
+      echo "  make tests"
+      echo ""
+      echo "Note: Do NOT use autoreconf - bash has a custom build system."
+      echo ""
+    '';
+
+    # Ensure the linker can find readline
+    NIX_LDFLAGS = "-L${pkgs.readline}/lib";
+    NIX_CFLAGS_COMPILE = "-I${pkgs.readline}/include";
   };
 }
