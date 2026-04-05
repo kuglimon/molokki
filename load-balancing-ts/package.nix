e2638f24a9c528e0587059852d4f65dd6502633a
Tatu Argillander <taatelio@gmail.com>
2026-04-05 10:57:41 +0300

    ts-load-balancing: migrate legacy pnpm build

--- load-balancing-ts/package.nix
+++ load-balancing-ts/package.nix
@@ -1,25 +1,31 @@
 {
   stdenv,
   pkgs,
+  pnpmConfigHook,
+  fetchPnpmDeps,
+  pnpm_10,
+  nodejs_22,
 }:
 let
+  pnpm' = pnpm_10.override { nodejs = nodejs_22; };
 in
 {
-
   package = stdenv.mkDerivation (finalAttrs: {
     pname = "load-balancing-ts";
-    version = "v0.0.1";
+    version = "v0.0.2";
 
     # TODO(tatu): Should never do this as, use filesets
     src = ./.;
 
-    nativeBuildInputs = with pkgs; [
-      nodejs
-      pnpm.configHook
+    nativeBuildInputs = [
+      nodejs_22
+      pnpm'
+      pnpmConfigHook
     ];
 
-    pnpmDeps = pkgs.pnpm.fetchDeps {
+    pnpmDeps = fetchPnpmDeps {
       inherit (finalAttrs) pname version src;
+      fetcherVersion = 1;
       hash = "sha256-8/MU4RkQNl3B4UvDlRRU+lz3t/c8TTeo4zIpkgN0wmc=";
     };
 
